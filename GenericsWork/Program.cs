ac4fccf277198ef79c054b2d57376027bac384f4
CAVİT <CAVT@DESKTOP-LTIRGEP>
2022-11-04 19:24:12 +0300

    04.11.2022 4. ders ödev

---
+++ GenericsWork/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericsWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary <string> ulkeler = new MyDictionary<string>();
+            ulkeler.Add("Türkiye");
+            ulkeler.Add("İngiltere");
+            ulkeler.Add("Fransa");
+            ulkeler.Add("Almanya");
+            ulkeler.Add("USA");
+
+        }
+    }
+}
